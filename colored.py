9b8a7e43db04da303b8bba430fb1ea0b8e5b86ea
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-02-15 09:46:58 -0600

    Allow it to work in Jupyter notebooks

--- colored.py
+++ colored.py
@@ -7,13 +7,18 @@ installed = None
 
 try:
     from termcolor import colored
-    colored = colored
     installed = True
 except:
     colored = not_colored
     installed = False
 
-if not sys.stdout.isatty():
+if hasattr(sys.stdout,"isatty"):
+    is_tty = sys.stdout.isatty()
+else:
+    is_tty = False
+
+is_jupyter = type(sys.stdout).__name__ == 'OutStream' and  type(sys.stdout).__module__ == 'ipykernel.iostream'
+if (not is_tty) and (not is_jupyter):
     colored = not_colored
 
 if __name__ == "__main__":
